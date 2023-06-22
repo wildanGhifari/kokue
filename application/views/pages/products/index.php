905d30d714d92daa4602fa787dba6536388106d9
wildanGhifari <wildghifari@gmail.com>
2023-06-22 09:57:38 +0700

    feat: add dummy products

--- application/views/pages/products/index.php
+++ application/views/pages/products/index.php
@@ -13,5 +13,119 @@
                 New Product
             </button>
         </div>
+        <div class="product-cards-container">
+            <div class="card grid-1" style="width: 18rem;">
+                <img src="<?= base_url('assets/imgs/img-placeholder.png') ?>" class="card-img-top" alt="Product image">
+                <div class="card-body">
+                    <h5 class="card-title">Chocolate</h5>
+                    <p class="card-text">Rp. 150.000</p>
+                </div>
+            </div>
+            <div class="card grid-2" style="width: 18rem;">
+                <img src="<?= base_url('assets/imgs/img-placeholder.png') ?>" class="card-img-top" alt="Product image">
+                <div class="card-body">
+                    <h5 class="card-title">Chocolate</h5>
+                    <p class="card-text">Rp. 150.000</p>
+                </div>
+            </div>
+            <div class="card grid-3" style="width: 18rem;">
+                <img src="<?= base_url('assets/imgs/img-placeholder.png') ?>" class="card-img-top" alt="Product image">
+                <div class="card-body">
+                    <h5 class="card-title">Chocolate</h5>
+                    <p class="card-text">Rp. 150.000</p>
+                </div>
+            </div>
+            <div class="card grid-4" style="width: 18rem;">
+                <img src="<?= base_url('assets/imgs/img-placeholder.png') ?>" class="card-img-top" alt="Product image">
+                <div class="card-body">
+                    <h5 class="card-title">Chocolate</h5>
+                    <p class="card-text">Rp. 150.000</p>
+                </div>
+            </div>
+            <div class="card grid-5" style="width: 18rem;">
+                <img src="<?= base_url('assets/imgs/img-placeholder.png') ?>" class="card-img-top" alt="Product image">
+                <div class="card-body">
+                    <h5 class="card-title">Chocolate</h5>
+                    <p class="card-text">Rp. 150.000</p>
+                </div>
+            </div>
+            <div class="card grid-6" style="width: 18rem;">
+                <img src="<?= base_url('assets/imgs/img-placeholder.png') ?>" class="card-img-top" alt="Product image">
+                <div class="card-body">
+                    <h5 class="card-title">Chocolate</h5>
+                    <p class="card-text">Rp. 150.000</p>
+                </div>
+            </div>
+            <div class="card grid-7" style="width: 18rem;">
+                <img src="<?= base_url('assets/imgs/img-placeholder.png') ?>" class="card-img-top" alt="Product image">
+                <div class="card-body">
+                    <h5 class="card-title">Chocolate</h5>
+                    <p class="card-text">Rp. 150.000</p>
+                </div>
+            </div>
+            <div class="card grid-8" style="width: 18rem;">
+                <img src="<?= base_url('assets/imgs/img-placeholder.png') ?>" class="card-img-top" alt="Product image">
+                <div class="card-body">
+                    <h5 class="card-title">Chocolate</h5>
+                    <p class="card-text">Rp. 150.000</p>
+                </div>
+            </div>
+            <div class="card grid-9" style="width: 18rem;">
+                <img src="<?= base_url('assets/imgs/img-placeholder.png') ?>" class="card-img-top" alt="Product image">
+                <div class="card-body">
+                    <h5 class="card-title">Chocolate</h5>
+                    <p class="card-text">Rp. 150.000</p>
+                </div>
+            </div>
+            <div class="card grid-10" style="width: 18rem;">
+                <img src="<?= base_url('assets/imgs/img-placeholder.png') ?>" class="card-img-top" alt="Product image">
+                <div class="card-body">
+                    <h5 class="card-title">Chocolate</h5>
+                    <p class="card-text">Rp. 150.000</p>
+                </div>
+            </div>
+            <div class="card grid-11" style="width: 18rem;">
+                <img src="<?= base_url('assets/imgs/img-placeholder.png') ?>" class="card-img-top" alt="Product image">
+                <div class="card-body">
+                    <h5 class="card-title">Chocolate</h5>
+                    <p class="card-text">Rp. 150.000</p>
+                </div>
+            </div>
+            <div class="card grid-12" style="width: 18rem;">
+                <img src="<?= base_url('assets/imgs/img-placeholder.png') ?>" class="card-img-top" alt="Product image">
+                <div class="card-body">
+                    <h5 class="card-title">Chocolate</h5>
+                    <p class="card-text">Rp. 150.000</p>
+                </div>
+            </div>
+            <div class="card grid-13" style="width: 18rem;">
+                <img src="<?= base_url('assets/imgs/img-placeholder.png') ?>" class="card-img-top" alt="Product image">
+                <div class="card-body">
+                    <h5 class="card-title">Chocolate</h5>
+                    <p class="card-text">Rp. 150.000</p>
+                </div>
+            </div>
+            <div class="card grid-14" style="width: 18rem;">
+                <img src="<?= base_url('assets/imgs/img-placeholder.png') ?>" class="card-img-top" alt="Product image">
+                <div class="card-body">
+                    <h5 class="card-title">Chocolate</h5>
+                    <p class="card-text">Rp. 150.000</p>
+                </div>
+            </div>
+            <div class="card grid-15" style="width: 18rem;">
+                <img src="<?= base_url('assets/imgs/img-placeholder.png') ?>" class="card-img-top" alt="Product image">
+                <div class="card-body">
+                    <h5 class="card-title">Chocolate</h5>
+                    <p class="card-text">Rp. 150.000</p>
+                </div>
+            </div>
+            <div class="card grid-16" style="width: 18rem;">
+                <img src="<?= base_url('assets/imgs/img-placeholder.png') ?>" class="card-img-top" alt="Product image">
+                <div class="card-body">
+                    <h5 class="card-title">Chocolate</h5>
+                    <p class="card-text">Rp. 150.000</p>
+                </div>
+            </div>
+        </div>
     </div>
 </main>
